f45664cec20083f22340b98292157517c594239b
unknown <mdtauhidulislam509@gmail.com>
2022-05-25 01:39:56 +0600

    my order delete

--- index.js
+++ index.js
@@ -95,6 +95,15 @@ async function run() {
          
         
     })
+
+
+     //------------- Delete order from myorder---------------
+     app.delete('/myorders/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await orderCollection.deleteOne(query);
+        res.send(result);
+    });
         
 
       
